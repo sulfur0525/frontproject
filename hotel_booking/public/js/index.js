76d41c5ceac947aeeb8f660f7a6968530550a18a
kimy <kimy@localhost>
2023-02-03 00:31:02 +0900

    0203_헤더, 푸터 구현중

--- hotel_booking/public/js/index.js
+++ hotel_booking/public/js/index.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+console.log('ㅎㅇ')
+document.querySelector('.dining').addEventListener( 'click' , ()=>{
+	document.querySelector('.modal_wrap').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener( 'click' , ()=>{
+	document.querySelector('.modal_wrap').style.display = 'none';
+})
