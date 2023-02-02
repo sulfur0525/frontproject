d6c850fd9ab0fc226424d0b2fa09c14ae2c66c74
김지호 <sulfur0525@nave.com>
2023-02-02 17:56:12 +0900

    2023.02.02 5:55 사이드바(95%)완료

--- hotel_booking/public/js/admin/admin.js
+++ hotel_booking/public/js/admin/admin.js
@@ -6,8 +6,47 @@ document.querySelector('.adminpage').addEventListener('click', (e)=>{
 /*홈을 누르면 사이드바2 사라지게 하기*/
 document.querySelector('.home').addEventListener('click', (e)=>{
 	document.querySelector('.navbox2').style.display = 'none'
+	document.querySelector('.sub1').style.display = 'none'
+	document.querySelector('.sub2').style.display = 'none'
+	document.querySelector('.sub3').style.display = 'none'
+	document.querySelector('.sub4').style.display = 'none'
+	document.querySelector('.sub5').style.display = 'none'
 })
 
+/* 사이드바2 헤드메뉴 누르면 서브메뉴 나타나게 하기*/
 document.querySelector('.title1').addEventListener('click', (e)=>{
-	document.querySelector('.sub_menus').style.display = 'block'
+	document.querySelector('.sub1').style.display = 'block'
+	document.querySelector('.sub2').style.display = 'none'
+	document.querySelector('.sub3').style.display = 'none'
+	document.querySelector('.sub4').style.display = 'none'
+	document.querySelector('.sub5').style.display = 'none'
+	
+})
+document.querySelector('.title2').addEventListener('click', (e)=>{
+	document.querySelector('.sub2').style.display = 'block'
+	document.querySelector('.sub1').style.display = 'none'
+	document.querySelector('.sub3').style.display = 'none'
+	document.querySelector('.sub4').style.display = 'none'
+	document.querySelector('.sub5').style.display = 'none'
+})
+document.querySelector('.title3').addEventListener('click', (e)=>{
+	document.querySelector('.sub3').style.display = 'block'
+	document.querySelector('.sub1').style.display = 'none'
+	document.querySelector('.sub2').style.display = 'none'
+	document.querySelector('.sub4').style.display = 'none'
+	document.querySelector('.sub5').style.display = 'none'
+})
+document.querySelector('.title4').addEventListener('click', (e)=>{
+	document.querySelector('.sub4').style.display = 'block'
+	document.querySelector('.sub1').style.display = 'none'
+	document.querySelector('.sub3').style.display = 'none'
+	document.querySelector('.sub2').style.display = 'none'
+	document.querySelector('.sub5').style.display = 'none'
+})
+document.querySelector('.title5').addEventListener('click', (e)=>{
+	document.querySelector('.sub5').style.display = 'block'
+	document.querySelector('.sub1').style.display = 'none'
+	document.querySelector('.sub3').style.display = 'none'
+	document.querySelector('.sub4').style.display = 'none'
+	document.querySelector('.sub2').style.display = 'none'
 })
