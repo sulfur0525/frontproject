2f7c11165fa3e077092162904c3e554478e8c60e
dbwl9 <dbwl9@DESKTOP-STKA34T>
2023-02-06 08:43:35 +0900

    02/06 js 수정

--- hotel_booking/public/js/member/login.js
+++ hotel_booking/public/js/member/login.js
@@ -1,3 +1,24 @@
-/**
- * 
- */
+
+let member = {id : 'qwer' , pw : 1234}
+
+let b_loginbtn = document.querySelector('.b_loginbtn')
+let b_Signupbtn = document.querySelector('.b_Signupbtn')
+		
+//로그인버튼 클릭 함수
+function loginbtn(){
+	
+	let login = {
+		id : document.querySelector('.b_loginId').value , 
+		pw : document.querySelector('.b_loginPw').value}
+
+	
+		if( member.id != login.id || login.id == ""){
+		alert('아이디를 확인 해주세요.');
+		}else if(member.pw != login.pw || login.pw == ""){
+		alert('비밀번호를 확인 해주세요.');
+		}else {alert('로그인 성공')}
+
+
+}
+//회원가입 - 가입완료 버튼 클릭 이벤트
+
--- hotel_booking/public/js/member/signup.js
+++ hotel_booking/public/js/member/signup.js
@@ -1,3 +1,51 @@
-/**
- * 
- */
+let b_Signupbtn = document.querySelector('.b_Signupbtn')
+
+b_Signupbtn.addEventListener('click',(e)=>{
+	let b_check = document.querySelector('.b_check').value;
+	let b_pwcheck = document.querySelector('.b_pwcheck').value;
+	let b_pwRecheck = document.querySelector('.b_pwRecheck').value;
+	
+	singup = {
+		id : b_check,
+		pw : b_pwcheck		
+	}
+	
+	let check = true;
+	
+	if(b_check == "" && b_pwcheck == "" && b_pwRecheck == ""){
+		alert('필수정보를 확인 해주세요') ; check = false;
+	}else if (b_check == b_loginId){
+		alert('이미 등록 된 아이디입니다.'); check = false;
+	}else if (b_check != b_pwcheck){
+		alert('비밀번호가 다릅니다.'); check = false;
+	}
+	
+	if(check){ member.push(signup) ; alert('회원가입 되었습니다.') }
+	
+})
+
+let b_Signupbtn = document.querySelector('.b_Signupbtn')
+
+b_Signupbtn.addEventListener('click',(e)=>{
+	let b_check = document.querySelector('.b_check').value;
+	let b_pwcheck = document.querySelector('.b_pwcheck').value;
+	let b_pwRecheck = document.querySelector('.b_pwRecheck').value;
+	
+	singup = {
+		id : b_check,
+		pw : b_pwcheck		
+	}
+	
+	let check = true;
+	
+	if(b_check == "" && b_pwcheck == "" && b_pwRecheck == ""){
+		alert('필수정보를 확인 해주세요') ; check = false;
+	}else if (b_check == b_loginId){
+		alert('이미 등록 된 아이디입니다.'); check = false;
+	}else if (b_check != b_pwcheck){
+		alert('비밀번호가 다릅니다.'); check = false;
+	}
+	
+	if(check){ member.push(signup) ; alert('회원가입 되었습니다.') }
+	
+})
