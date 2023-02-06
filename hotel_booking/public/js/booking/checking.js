e27b5516a004f6ce757c1a2852173ea2400f921d
katonew <katonew98@gmail.com>
2023-02-06 14:28:29 +0900

    2/6 14:28

--- hotel_booking/public/js/booking/checking.js
+++ hotel_booking/public/js/booking/checking.js
@@ -4,12 +4,25 @@ let booking = [
 	{no: '1234', room: '슈페리어', firstyear: 2023, firstmouth : 2, firstdate: 14, lastyear: 2023, lastmouth: 2, lastdate: 16, price: 250000},
 	{no: '5678', room: '슈페리어 디럭스', firstyear: 2023, firstmouth : 3, firstdate: 2, lastyear: 2023, lastmouth: 3, lastdate: 7, price: 600000},
 	{no: '9012', room: '주니어 스위트 킹', firstyear: 2023, firstmouth : 2, firstdate: 16, lastyear: 2023, lastmouth: 2, lastdate: 19, price: 350000},
-	
-	
 ]
 
 
 function checkingbtn(){
-	let inputno = document.querySelector('.c_checkongNo').value
-	console.log(inputno)
+	let inputno = document.querySelector('.c_checkingNo').value
+	document.querySelector('.checkingbox').style.display = 'block'
+	for(let i=0;i<booking.length;i++){
+		if(booking[i].no==inputno){
+			document.querySelector('.c_resultbox').innerHTML = `<h2>예약하신 정보</h2>`
+			document.querySelector('.c_firstday').innerHTML = 
+			`${booking[i].firstyear}년 ${booking[i].firstmouth}월 ${booking[i].firstdate}일`
+			document.querySelector('.c_lastday').innerHTML = 
+			`${booking[i].lastyear}년 ${booking[i].lastmouth}월 ${booking[i].lastdate}일`
+			document.querySelector('.c_price').innerHTML = `${booking[i].price.toLocaleString()}원`
+			break;
+		}else{
+			document.querySelector('.c_resultbox').innerHTML = `<h2>일치하는 예약번호가 없습니다.</h2>`
+			document.querySelector('.c_firstday').innerHTML = ``
+			document.querySelector('.c_lastday').innerHTML = ``
+		}
+	}
 }
