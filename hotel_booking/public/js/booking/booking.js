c1fdb6e7b524ec3b62ed85a39088cfe71e896778
katonew <katonew98@gmail.com>
2023-02-06 11:09:57 +0900

    02/06 11:09

--- hotel_booking/public/js/booking/booking.js
+++ hotel_booking/public/js/booking/booking.js
@@ -22,19 +22,10 @@ let last_date = first_date+1
 
 
 
-selectlist()
 first_print()
 last_print()
-
 bookingdate()
-print_room('슈페리어')
 
-//상품타입 선택 버튼
-function option_select(){
-	document.querySelector('.c_roomoption').style.display = 'block'
-	let selectroom =document.querySelector('.c_select_room').value
-	print_room(selectroom)
-}
 // 인원수 줄이기 버튼
 function prevbtn(i){
 	if(i==1){
@@ -64,7 +55,6 @@ function first_print(){
 	let year = first_year
 	let month = first_month
 	let selectday = first_date
-	
 	// 3. html '요일' 구성
 	let html = `<div class="c_day weekday sunday">일</div>
 				<div class="c_day weekday">월</div>
@@ -73,12 +63,8 @@ function first_print(){
 				<div class="c_day weekday">목</div>
 				<div class="c_day weekday">금</div>
 				<div class="c_day weekday">토</div>`
-				
-		// * 1.현재 설정된 월의 마지막 일 구하는 방법
 	let lastday = new Date(year, month, 0).getDate();
-		// * 2. 현재 캘린더 설정된 날짜의 1일 시작요일 구하기
 	let weekday = new Date(year, month-1, 1).getDay();
-		//현재 날짜의 요일
 	
 	//console.log(weekday);
 	
@@ -200,74 +186,10 @@ function bookingdate(){
 		first_print()
 		bookingdate()
 	}else{
-		document.querySelector('.c_bookingdate').innerHTML = `총 숙박기간 : ${diffDate}일`
+		document.querySelector('.c_bookingdate').innerHTML = `총 : ${diffDate}일`
+		document.querySelector('.c_bookingdate2').innerHTML = `총 : ${diffDate}일`
 	}
 }
-
-//객실 옵션 출력 함수
-function print_room(x){
-	for(let i=0;i<room.length;i++){
-		let selectprice = 0;
-		if(adultno>=3){
-			selectprice = room[i].price+(room[i].plusPrice*(adultno-2))
-		}
-		else{
-			selectprice = room[i].price
-		}
-		if(x==room[i].name){
-			document.querySelector('.c_selectinfo').innerHTML = `
-				<div class="c_selectRoomName" value="${room[i].name}">${room[i].name}</div>
-				<div class="c_selectPrice" value="${selectprice.toLocaleString()}">${selectprice.toLocaleString()}</div>`
-			document.querySelector('.c_info_imagebox').innerHTML = `<img class="c_info_image" src="../img/booking/${room[i].img}">`
-		}
-	}
-}
-//장바구니 버튼클릭 함수
-function selectlistbtn(){
-	let name = document.querySelector('.c_selectRoomName').value
-	let price = document.querySelector('.c_selectPrice').value
-	console.log(name)
-	console.log(price)
-	let templist= {
-		name : document.querySelector('.c_selectRoomName').value,
-		price : document.querySelector('.c_selectPrice').value,
-		firstday : {
-						first_year : first_year, 
-						first_month : first_month,
-						first_date : first_date
-					},
-		lastday : {
-						last_year : last_year, 
-						last_month : last_month,
-						last_date : last_date
-					}
-	}
-	bookinglist.push(templist)
-	console.log(bookinglist)
-	selectlist()
-}
-
-//장바구니 출력함수
-function selectlist(){
-	
-	let html = ``
-	for(let i=0;i<bookinglist.length;i++){
-		html += `<div class="selectlist(i)">
-				<div>
-					<h5 >예약 날짜</h5>
-					<span class="c_boxFirst">${first_year}/${first_month}/${first_date}</span>
-					<span>-</span>
-					<span class="c_boxLast">${last_year}/${last_month}/${last_date}</span>
-				</div>
-				<div class="selectitem">
-					<h5 class="c_selectlistname">객실이름</h5>
-					<h5 class="c_selectprice">금액</h5>
-				</div>`
-	}
-	
-	document.querySelector('.c_selectListAll').innerHTML = html;
-}
-
 
 function c_modal_write(){
 	first_print()	
---
+++ hotel_booking/public/js/booking/checking.js
@@ -0,0 +1,15 @@
+
+
+let booking = [
+	{no: '1234', room: '슈페리어', firstyear: 2023, firstmouth : 2, firstdate: 14, lastyear: 2023, lastmouth: 2, lastdate: 16, price: 250000},
+	{no: '5678', room: '슈페리어 디럭스', firstyear: 2023, firstmouth : 3, firstdate: 2, lastyear: 2023, lastmouth: 3, lastdate: 7, price: 600000},
+	{no: '9012', room: '주니어 스위트 킹', firstyear: 2023, firstmouth : 2, firstdate: 16, lastyear: 2023, lastmouth: 2, lastdate: 19, price: 350000},
+	
+	
+]
+
+
+function checkingbtn(){
+	let inputno = document.querySelector('.c_checkongNo').value
+	console.log(inputno)
+}
