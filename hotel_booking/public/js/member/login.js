468bb96aae6ae77f264809d1988a9223079dbf22
504 <504@DESKTOP-DBOC81R>
2023-02-06 16:51:29 +0900

    02/06 16:50

--- hotel_booking/public/js/member/login.js
+++ hotel_booking/public/js/member/login.js
@@ -20,11 +20,19 @@ function loginbtn(){
 		}else if(member.pw != login.pw || login.pw == ""){
 		alert('비밀번호를 확인 해주세요.');
 		}else {alert('로그인 성공') }
+}
 
+function adminloginbtn(){
+	
+	let login = {
+		id : document.querySelector('.admin_id').value , 
+		pw : document.querySelector('.admin_pw').value}	
+	
 		if( login.id != admin.id){
 		alert('관리자 아이디를 확인 해주세요.');
 		}else if(login.pw != admin.pw){
 		alert('관리자 비밀번호를 확인 해주세요.');
 		}else {alert('관리자 로그인 성공') ; location.href="../admin/admin_home.html";}
 
+	
 }
