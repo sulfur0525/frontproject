8eb61eb1179daea8fc9451c219fa089103f04bdc
김지호 <sulfur0525@nave.com>
2023-02-07 12:04:04 +0900

    2023.02.07

--- hotel_booking/public/js/admin/admin_realtime.js
+++ hotel_booking/public/js/admin/admin_realtime.js
@@ -67,49 +67,100 @@ function room_list(){
 
 function bo_modal(i){ 
 	document.querySelector('.e_real_modal').style.display = 'flex'
-	
+	let html = ''
 	let 요금 = 0;
 	
+	
+	
+	let have = false
+	let k=0;
+	
+	booking.forEach((j,p)=>{
+		if(rooms[i].호수==j.호수){
+			have=true
+			k=p
+			
+		}
+	})
+	
 	roomtype.forEach((j)=>{
-		if(booking[i].객실타입==j.객실타입){
+		if(booking[k].객실타입==j.객실타입){
 			요금=j.가격
 		}
 	})
 	
-	
-	
-	let html = `<thead>
+	if(have){
+		html = `<thead>
 						<tr>
-							<th class="e_rervnum" colspan="5">${booking[i].예약번호}</th>
+							<th class="e_rervnum" colspan="5">${booking[k].예약번호}</th>
 							<th id="e_close" onclick="close_modal()">X</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr>
-							<th id="e_roomtype" colspan="6">${booking[i].호수} ${booking[i].객실타입}</th>
+							<th id="e_roomtype" colspan="6">${rooms[i].호수} ${rooms[i].객실타입}</th>
 						</tr>
 						<tr>
 							<th width="10%">고객명 </th> 
-							<td width="23%"><input class="e_bo_name" type="text" placeholder="${booking[i].고객명}"></td>
+							<td width="23%"><input class="e_bo_name" type="text" placeholder="${booking[k].고객명}"></td>
 							<th width="10%">입실일자 </th> 
-							<td width="23%"><input class="e_bo_date" type="text" placeholder="${booking[i].입실날짜}"></td>
+							<td width="23%"><input class="e_bo_date" type="text" placeholder="${booking[k].입실날짜}"></td>
 							<th width="10%">연락처 </th> 
-							<td width="23%"><input class="e_bo_number" type="text" placeholder="${booking[i].전화번호}"></td>
+							<td width="23%"><input class="e_bo_number" type="text" placeholder="${booking[k].전화번호}"></td>
 						</tr>
 						<tr>
 							<th width="10%">인원 </th> 
-							<td width="23%"><input class="e_bo_p" type="text" placeholder="${booking[i].인원}"></td>
+							<td width="23%"><input class="e_bo_p" type="text" placeholder="${booking[k].인원}"></td>
 							<th width="10%">숙박일수</th> 
-							<td width="23%"><input class="e_bo_stay" type="text" placeholder="${booking[i].숙박일수}"></td>
+							<td width="23%"><input class="e_bo_stay" type="text" placeholder="${booking[k].숙박일수}"></td>
 							<th width="10%">비고 </th> 
-							<td width="23%"><input class="e_bo_please" type="text" placeholder="${booking[i].요청사항}"></td>
+							<td width="23%"><input class="e_bo_please" type="text" placeholder="${booking[k].요청사항}"></td>
 						</tr>
 						<tr>
 							<th width="10%">총 요금 </th>
 							<td width="23%"><input type="text" placeholder="${요금*booking[i].숙박일수}" disabled="disabled"></td>
-							<td class="e_b_ch" colspan="4"><button type="button" onclick="e_ch_butt(${i})" class="e_ch_but">변경</button></td>
+							<td class="e_b_ch" colspan="4">
+								<button type="button" onclick="e_ch_butt(${k})" class="e_ch_but">변경</button>
+								<button type="button" onclick="e_in_butt(${k,i})" class="e_ch_but">입실</button>
+							</td>
 						</tr>
 					</tbody>`
+	}
+	else{
+		html = `<thead>
+						<tr>
+							<th class="e_rervnum" colspan="5">  </th>
+							<th id="e_close" onclick="close_modal()">X</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th id="e_roomtype" colspan="6">${rooms[i].호수} ${rooms[i].객실타입}</th>
+						</tr>
+						<tr>
+							<th width="10%">고객명 </th> 
+							<td width="23%"><input class="e_bo_name" type="text" placeholder=""></td>
+							<th width="10%">입실일자 </th> 
+							<td width="23%"><input class="e_bo_date" type="text" placeholder=""></td>
+							<th width="10%">연락처 </th> 
+							<td width="23%"><input class="e_bo_number" type="text" placeholder=""></td>
+						</tr>
+						<tr>
+							<th width="10%">인원 </th> 
+							<td width="23%"><input class="e_bo_p" type="text" placeholder=""></td>
+							<th width="10%">숙박일수</th> 
+							<td width="23%"><input class="e_bo_stay" type="text" placeholder=""></td>
+							<th width="10%">비고 </th> 
+							<td width="23%"><input class="e_bo_please" type="text" placeholder=""></td>
+						</tr>
+						<tr>
+							<th width="10%">총 요금 </th>
+							<td width="23%"><input type="text" placeholder="" disabled="disabled"></td>
+							<td class="e_b_ch" colspan="4">
+							<button type="button" onclick="e_chin_butt(${i})" class="e_ch_but">변경</button></td>
+						</tr>
+					</tbody>`
+	}
 					
 	document.querySelector('.e_modal_table').innerHTML = html
 }
@@ -119,9 +170,62 @@ function close_modal(){
 	document.querySelector('.e_real_modal').style.display = 'none'
 }
 
+function e_ch_butt(i){
+	console.log("작동?"+i)
+	let e_bo_name = document.querySelector('.e_bo_name').value
+	let e_bo_date = document.querySelector('.e_bo_date').value
+	let e_bo_number = document.querySelector('.e_bo_number').value
+	let e_bo_p = document.querySelector('.e_bo_p').value
+	let e_bo_stay = document.querySelector('.e_bo_stay').value
+	let e_bo_please = document.querySelector('.e_bo_please').value
+	
+	if(e_bo_name!=''){
+		booking[i].고객명=e_bo_name
+	}
+	if(e_bo_date!=''){
+		booking[i].입실날짜=e_bo_date
+	}
+	if(e_bo_number!=''){
+		booking[i].전화번호=e_bo_number
+	}
+	if(e_bo_p!=''){
+		booking[i].인원=e_bo_p
+	}
+	if(e_bo_stay!=''){
+		booking[i].숙박일수=e_bo_stay
+	}
+	if(e_bo_please!=''){
+		booking[i].요청사항=e_bo_please
+	}
+	
+	document.querySelector('.e_real_modal').style.display = 'none'
+	room_list()
+}
 
+function e_chin_butt(i){
+	console.log("작동?"+i)
+	let e_bo_name = document.querySelector('.e_bo_name').value
+	let e_bo_date = document.querySelector('.e_bo_date').value
+	let e_bo_number = document.querySelector('.e_bo_number').value
+	let e_bo_p = document.querySelector('.e_bo_p').value
+	let e_bo_stay = document.querySelector('.e_bo_stay').value
+	let e_bo_please = document.querySelector('.e_bo_please').value
+	
+	if(e_bo_name!=''&&e_bo_date!=''&&e_bo_number!=''&&e_bo_p!=''&&e_bo_stay!=''&&e_bo_please!=''){
+		booking.push({예약번호: (booking[booking.length-1].예약번호+1) , 고객명: e_bo_name, 전화번호: e_bo_number, 객실타입: rooms[i].객실타입, 호수: rooms[i].호수 ,입실날짜: e_bo_date, 숙박일수: e_bo_stay, 요청사항:e_bo_please, 인원:e_bo_p})
+		
+		document.querySelector('.e_real_modal').style.display = 'none'
+		room_list()
+	}
+	else{alert('모든 내용을 채워주십시오.')}
 
+}
 
+function e_in_butt(k){
+	realtime.push({호수: booking[k].호수 , 예약번호: booking[k].예약번호 , 객실타입: booking[k].객실타입, 고객명: booking[k].고객명 , 인원: booking[k].인원 })
+	document.querySelector('.e_real_modal').style.display = 'none'
+	room_list()
+}
 
 /*document.querySelector('.e_wide').addEventListener(('click'),()=>{
 	document.querySelector('.e_navbox1').style.display = 'none'
